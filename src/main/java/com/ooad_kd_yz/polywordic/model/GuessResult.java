6277ee0d157549d61d366ec40029021928192a8b
Katherine Deegan <katdeegan16@att.net>
2025-11-19 17:26:30 -0700

    first Java class to define GuessResult object

--- src/main/java/com/ooad_kd_yz/polywordic/model/GuessResult.java
+++ src/main/java/com/ooad_kd_yz/polywordic/model/GuessResult.java
@@ -1,4 +1,13 @@
 package com.ooad_kd_yz.polywordic.model;
 
+// Represents the result of the player's letter guess in Polywordic
+// GuessResult values include:
+//  CORRECT: Letter in correct position (green tile)
+//  PRESENT: Letter in word, but wrong position (yellow tile)
+//  ABSENT: Letter not in word (gray tile)
+
 public enum GuessResult {
+    CORRECT,
+    PRESENT,
+    ABSENT
 }
