7deafbcd5722cb56a674b73bc2dc528e22b6d06c
Katherine Deegan <katdeegan16@att.net>
2025-11-30 13:53:31 -0700

    repository for 5 letter words for game data

--- src/main/java/com/ooad_kd_yz/polywordic/repository/IWordRepository.java
+++ src/main/java/com/ooad_kd_yz/polywordic/repository/IWordRepository.java
@@ -1,4 +1,9 @@
 package com.ooad_kd_yz.polywordic.repository;
 
+import java.util.List;
+
 public interface IWordRepository {
+    String getRandomWord();
+    boolean isValidWord(String word);
+    List<String> getAllWords();
 }
